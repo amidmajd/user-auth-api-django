9caf0d38781ba260fea2d2b20b20c229f0dc585f
Amid Majd <amidmajd@gmail.com>
2021-11-16 12:58:57 +0330

    users db model

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = None
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birthday = models.DateField()
+    phone_number = models.CharField(max_length=11, validators=[RegexValidator(r'^\d{11}$')])
+    address = models.TextField(max_length=300)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
