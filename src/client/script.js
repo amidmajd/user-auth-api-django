761b6f59fa1256120bb14451fa5f57fa4e504e71
Amid Majd <amidmajd@gmail.com>
2021-11-17 20:26:24 +0330

    new style + navigation

--- src/client/script.js
+++ src/client/script.js
@@ -1,31 +1,49 @@
 const BASE_URL = "http://localhost:8000";
 let CURRENT_USER_ID = undefined;
 
-let messageBox = document.getElementById("message-box");
+const toastBox = document.getElementById("live-toast");
+const toastMessage = document.getElementById("toast-message");
 
-let viewUserInfoBtn = document.getElementById("btn-viewUserInfo");
-let userInfoForm = document.getElementById("form-user-info");
+const navHomeBtn = document.getElementById("btn-nav-home");
 
-let viewAllUsersBtn = document.getElementById("btn-viewAllUsers");
-let allUsersTableBody = document.getElementById("all-users-table-body");
+const navRegisterBtn = document.getElementById("btn-nav-register");
+const registerForm = document.getElementById("form-register");
+const registerBtn = document.getElementById("form-register__btn");
 
-let registerBtn = document.getElementById("form-register__btn");
-let registerForm = document.getElementById("form-register");
+const navLoginBtn = document.getElementById("btn-nav-login");
+const loginForm = document.getElementById("form-login");
+const loginBtn = document.getElementById("form-login__btn");
 
-let loginBtn = document.getElementById("form-login__btn");
-let loginForm = document.getElementById("form-login");
+const navLogoutBtn = document.getElementById("btn-nav-logout");
+const logoutBtn = document.getElementById("btn-logout");
 
-let logoutBtn = document.getElementById("btn-logout");
+const navEditBtn = document.getElementById("btn-nav-edit");
+const editForm = document.getElementById("form-edit");
+const editBtn = document.getElementById("form-edit__btn");
 
-let editBtn = document.getElementById("btn-editUser");
-let formEditBtn = document.getElementById("form-edit__btn");
-let editForm = document.getElementById("form-edit");
+const navUserInfoBtn = document.getElementById("btn-nav-user-info");
+const UserInfoTableBody = document.getElementById("user-info-table-body");
 
+const navAllUsersBtn = document.getElementById("btn-nav-all-users");
+const allUsersTableBody = document.getElementById("all-users-table-body");
+
+const ALL_SECTION_NAMES = {
+  "section-home": document.getElementById("section-home"),
+  "section-register": document.getElementById("section-register"),
+  "section-login": document.getElementById("section-login"),
+  "section-edit": document.getElementById("section-edit"),
+  "section-user-info": document.getElementById("section-user-info"),
+  "section-all-users": document.getElementById("section-all-users"),
+};
+
+const ALL_NAV_BTNS = [navRegisterBtn, navLoginBtn, navLogoutBtn, navEditBtn, navUserInfoBtn, navAllUsersBtn];
+
+//
+// Utils
 let messageBoxShow = (message) => {
-  messageBox.innerHTML = message;
-  setTimeout(() => {
-    messageBox.innerHTML = "";
-  }, 3000);
+  var toast = new bootstrap.Toast(toastBox);
+  toastMessage.textContent = message;
+  toast.show();
 };
 
 let errorhandler = (response, data) => {
@@ -41,7 +59,33 @@ let errorhandler = (response, data) => {
   }
 };
 
-// Logic
+let deActivateAllSections = () => {
+  for (const [section_name, section_dom] of Object.entries(ALL_SECTION_NAMES)) {
+    section_dom.classList.remove("section-active");
+  }
+};
+
+let deActivateAllNavBtns = () => {
+  for (const item of ALL_NAV_BTNS) {
+    item.classList.remove("active");
+  }
+};
+
+//
+// Logic & Flow
+navHomeBtn.addEventListener("click", (e) => {
+  deActivateAllSections();
+  deActivateAllNavBtns();
+  ALL_SECTION_NAMES["section-home"].classList.add("section-active");
+});
+
+navRegisterBtn.addEventListener("click", (e) => {
+  deActivateAllSections();
+  deActivateAllNavBtns();
+  ALL_SECTION_NAMES["section-register"].classList.add("section-active");
+  navRegisterBtn.classList.add("active");
+});
+
 registerBtn.addEventListener("click", async (e) => {
   try {
     let response = await fetch(BASE_URL + "/user/register", {
@@ -57,6 +101,13 @@ registerBtn.addEventListener("click", async (e) => {
   } catch (error) {
     messageBoxShow(error.message);
   }
+});
+
+navLoginBtn.addEventListener("click", (e) => {
+  deActivateAllSections();
+  deActivateAllNavBtns();
+  ALL_SECTION_NAMES["section-login"].classList.add("section-active");
+  navLoginBtn.classList.add("active");
 });
 
 loginBtn.addEventListener("click", async (e) => {
@@ -78,7 +129,7 @@ loginBtn.addEventListener("click", async (e) => {
   }
 });
 
-logoutBtn.addEventListener("click", async (e) => {
+navLogoutBtn.addEventListener("click", async (e) => {
   try {
     let response = await fetch(BASE_URL + "/user/logout", {
       method: "POST",
@@ -89,13 +140,17 @@ logoutBtn.addEventListener("click", async (e) => {
     await errorhandler(response, data);
 
     CURRENT_USER_ID = undefined;
+
+    deActivateAllSections();
+    deActivateAllNavBtns();
+
     messageBoxShow("Logged Out!");
   } catch (error) {
     messageBoxShow(error.message);
   }
 });
 
-viewUserInfoBtn.addEventListener("click", async (e) => {
+navUserInfoBtn.addEventListener("click", async (e) => {
   try {
     let response = await fetch(BASE_URL + "/user/user", {
       method: "GET",
@@ -105,11 +160,23 @@ viewUserInfoBtn.addEventListener("click", async (e) => {
     let data = await response.json();
     await errorhandler(response, data);
 
-    const { elements } = userInfoForm;
-    for (const [key, value] of Object.entries(data.data)) {
-      const field = elements.namedItem(key);
-      field && (field.value = value);
-    }
+    UserInfoTableBody.innerHTML = `
+    <tr>
+      <th scope="row">${data.data.id}</th>
+      <td>${data.data.email}</td>
+      <td>${data.data.first_name}</td>
+      <td>${data.data.last_name}</td>
+      <td>${data.data.gender}</td>
+      <td>${data.data.birthday}</td>
+      <td>${data.data.phone_number}</td>
+      <td>${data.data.address}</td>
+    </tr>
+    `;
+
+    deActivateAllSections();
+    deActivateAllNavBtns();
+    ALL_SECTION_NAMES["section-user-info"].classList.add("section-active");
+    navUserInfoBtn.classList.add("active");
 
     messageBoxShow("Fetched User Info!");
   } catch (error) {
@@ -117,7 +184,7 @@ viewUserInfoBtn.addEventListener("click", async (e) => {
   }
 });
 
-viewAllUsersBtn.addEventListener("click", async (e) => {
+navAllUsersBtn.addEventListener("click", async (e) => {
   try {
     let response = await fetch(BASE_URL + "/user/users", {
       method: "GET",
@@ -141,13 +208,18 @@ viewAllUsersBtn.addEventListener("click", async (e) => {
       allUsersTableBody.appendChild(tableRow);
     }
 
+    deActivateAllSections();
+    deActivateAllNavBtns();
+    ALL_SECTION_NAMES["section-all-users"].classList.add("section-active");
+    navAllUsersBtn.classList.add("active");
+
     messageBoxShow("Fetched All Users!");
   } catch (error) {
     messageBoxShow(error.message + ", maybe Login First!");
   }
 });
 
-editBtn.addEventListener("click", async (e) => {
+navEditBtn.addEventListener("click", async (e) => {
   try {
     let response = await fetch(BASE_URL + "/user/user", {
       method: "GET",
@@ -164,12 +236,17 @@ editBtn.addEventListener("click", async (e) => {
       const field = elements.namedItem(key);
       field && (field.value = value);
     }
+
+    deActivateAllSections();
+    deActivateAllNavBtns();
+    ALL_SECTION_NAMES["section-edit"].classList.add("section-active");
+    navEditBtn.classList.add("active");
   } catch (error) {
     messageBoxShow(error.message + ", maybe Login First!");
   }
 });
 
-formEditBtn.addEventListener("click", async (e) => {
+editBtn.addEventListener("click", async (e) => {
   try {
     let response = await fetch(BASE_URL + "/user/update/" + CURRENT_USER_ID, {
       method: "PUT",
